2908d9ed35d29086771809ae454dda28763021d1
ahmednaser111 <ahmednaser7707@gmail.com>
2024-11-19 10:54:01 +0200

    remove sqlite

--- src/app/features/FileTreeSlice.ts
+++ src/app/features/FileTreeSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { File, FileTab } from "../../interfaces";
-import { StatementSync } from "node:sqlite";
 
 export interface FileTreeState {
 	tabs: FileTab[];
